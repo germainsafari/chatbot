457d714e69e0bf4c673df3f9144fe95e814ee9ea
Safari Germain <sfrgermain@gmail.com>
2024-07-09 10:25:30 +0200

    trainings

--- python.py
+++ python.py
@@ -22,3 +22,25 @@
 
 import numpy
 
+'''
+NumPy ufuncs
+what are ufuncs - Universal functions and they are Numpy functions that operate on ndarray object
+
+there are used to implement vectorization in Numpy which is way faster than iterating over elements
+the also provide broadcasting and additional methods like reduce, accumulate that are helpful for computation
+ufuncs also take addtional arguments like
+where : boolean array or condition defining where the operations should take place
+dtype: defining the return type of elements
+out: output array where the return value should be copied.
+
+what is vectorization
+converting iterative statements into a vector based operation is called vectorization
+
+
+
+
+'''
+l = [1,2,3,4]
+k = [3,5,5,3]
+x = l + k
+print(x.sort())
