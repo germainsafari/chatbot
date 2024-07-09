9d02e358614fb2de7169edcfaa546597e91f0df8
Safari Germain <sfrgermain@gmail.com>
2024-07-09 10:10:24 +0200

    first commit

---
+++ python.py
@@ -0,0 +1,24 @@
+# # class Car:
+# #     def __init__(self, color, tires, lights, boot):
+# #         self.color = color
+# #         self.tires = tires
+# #         self.lights = lights
+# #         self.boot = boot
+
+# # class Bmw(Car):
+# #     def drive(self):
+# #         print('i am driving')
+
+# # bmw1 = Bmw('black', 4, 'red', 1 )
+# # bmw1.drive()
+
+# # user = input('enter your name:')
+# # print(user)
+# dic = {
+#     'name': 'Germain',
+#     'surname': 'Safari'
+# }
+# x = dic['name']
+
+import numpy
+
